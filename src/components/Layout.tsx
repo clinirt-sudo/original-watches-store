b36ed73eda99cc150ad188bd44d469f96cc9f5dc
clinirt-sudo <clinirt@gmail.com>
2026-05-30 12:21:37 +0100

    reset scroll position to top position on every route

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import CartDrawer from './CartDrawer';
 import FloatingButtons from './FloatingButtons';
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' as ScrollBehavior });
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
